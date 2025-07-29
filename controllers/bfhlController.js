3b66882aba450ce93f7ab8ece75b6d6508837fa4
Paramveer Singh Udawat <param11veerudawat@gmail.com>
2025-07-29 20:45:33 +0530

    Set up personal details

---
+++ controllers/bfhlController.js
@@ -0,0 +1,6 @@
+const {isNumber, isAlpha, alternatingCapsReverse} = require('../utils/parseData');
+
+const FULL_NAME = 'john_doe';
+const DOB = '17091999';
+const EMAIL = 'john@xyz.com';
+const ROLL_NUMBER = 'ABCD123';

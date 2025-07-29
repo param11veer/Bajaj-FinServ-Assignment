33ce6c719752d357e8bbf137e83481233aed8147
Paramveer Singh Udawat <param11veerudawat@gmail.com>
2025-07-29 20:48:22 +0530

    set up of basic schema using try catch

--- controllers/bfhlController.js
+++ controllers/bfhlController.js
@@ -4,3 +4,20 @@ const FULL_NAME = 'john_doe';
 const DOB = '17091999';
 const EMAIL = 'john@xyz.com';
 const ROLL_NUMBER = 'ABCD123';
+
+module.export = (req, res) => {
+    try{
+        const data = req.body.data || [];
+        let even_numbers = [];
+        let odd_numbers = [];
+        let alphabets = [];
+        let special_characters = [];
+        let all_alpha = [];
+        let sum = 0;
+
+
+    }
+    catch (error) {
+        res.json({is_success: false, error: error.message});
+    }
+};
